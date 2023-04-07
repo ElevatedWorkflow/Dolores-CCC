486637c19f388db46696f52993bb524a27bde986
Rigomortia <VitoReiter@gmail.com>
2023-04-07 11:08:38 -0400

    Updated to Index.js
    Worked further into the creation of channels, after getting the commands and subcommands to populate.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const client = new Client({
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  const guild = client.guilds.cache.get('GUILD_ID');
+  const guild = client.guilds.cache.get('GUILD_ID_HERE');
   if (!guild) return console.error('Guild not found');
 
   // Register the slash command for managing channels
@@ -24,12 +24,12 @@ client.on('ready', async () => {
       {
         name: 'create',
         description: 'Create a new channel',
-        type: 'SUB_COMMAND',
+        type: 1,
         options: [
           {
             name: 'name',
             description: 'Name of the new channel',
-            type: 'STRING',
+            type: 3,
             required: true,
           },
         ],
@@ -37,12 +37,12 @@ client.on('ready', async () => {
       {
         name: 'limit',
         description: 'Set the user limit for a channel',
-        type: 'SUB_COMMAND',
+        type: 1,
         options: [
           {
             name: 'int',
             description: 'User limit',
-            type: 'INTEGER',
+            type: 4,
             required: true,
           },
         ],
@@ -50,12 +50,12 @@ client.on('ready', async () => {
       {
         name: 'allow',
         description: 'Allow a user to access the channel',
-        type: 'SUB_COMMAND',
+        type: 1,
         options: [
           {
             name: 'user',
             description: 'User to allow',
-            type: 'USER',
+            type: 9,
             required: true,
           },
         ],
@@ -63,12 +63,12 @@ client.on('ready', async () => {
       {
         name: 'kick',
         description: 'Kick a user from the channel',
-        type: 'SUB_COMMAND',
+        type: 1,
         options: [
           {
             name: 'user',
             description: 'User to kick',
-            type: 'USER',
+            type: 9,
             required: true,
           },
         ],
@@ -88,6 +88,30 @@ client.on('interactionCreate', async (interaction) => {
     switch (subcommand) {
       case 'create':
         // Handle channel creation
+        const channelName = options.getString('name');
+        const everyoneRole = interaction.guild.roles.everyone;
+
+        const channel = await interaction.guild.channels.create(channelName, {
+          type: 'GUILD_TEXT',
+          permissionOverwrites: [
+          {
+            id: everyoneRole.id,
+            deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+          },
+          {
+            id: interaction.user.id,
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+          },
+        ],
+      });
+
+        await interaction.reply(`Channel "${channelName}" created!`);
+        const instructions = `To manage your channel, use the following commands:\n
+        - /channel limit {int}: Set the user limit for the channel\n
+        - /channel allow {user}: Allow a user to access the channel\n
+        - /channel kick {user}: Kick a user from the channel`;
+
+        await interaction.user.send(instructions);
         break;
       case 'limit':
         // Handle setting channel user limit
