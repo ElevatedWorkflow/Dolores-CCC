195328cdda41956b461a8015ef91032d8632c7b6
ElevatedWorkflow <JasonParserEW@gmail.com>
2023-04-12 16:17:01 -0400

    fix: removed unnecessary bottoken assignment

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const openai = require('openai');
 openai.apiKey = process.env.OPENAI_API_KEY;
-const botToken = process.env.BOT_TOKEN;
 
 const ClientService = require('./src/service/library/discordjs/client.service');
-const clientService = new ClientService(botToken, openai);
+const clientService = new ClientService(process.env.BOT_TOKEN, openai);
 
 clientService.init();
