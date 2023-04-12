9f1e54203546527c3cb0fc14689e1cb869e08d2a
ElevatedWorkflow <JasonParserEW@gmail.com>
2023-04-12 15:38:56 -0400

    fix: class refactors

--- src/service/base/service.base.js
+++ src/service/base/service.base.js
@@ -2,10 +2,12 @@ const ClientService = require('../../service/system/client.service');
 const LoggerService = require('./logger.service');
 const MessageService = require('./message.service');
 const ConfigService = require('../system/config.service');
+const GuildService = require('../library/discordjs/guild.service');
+const MessageHandlerService = require('../system/messagehandler.service');
 
 class BaseService {
   constructor() {
-    this.Client = new ClientService()
+    this.Client = new ClientService(new MessageHandlerService(), new GuildService())
     this.Logger = LoggerService;
     this.Message = MessageService;
     this.Config = new ConfigService(this.Logger);
--- src/service/library/discordjs/client.service.js
+++ src/service/library/discordjs/client.service.js
@@ -1,19 +1,39 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const MessageHandler = require('./MessageHandler');
 const BaseService = require('../../base/service.base');
-const GuildService = require('./guild.service');
-const messageHandler = new MessageHandler();
 
 class ClientService extends BaseService {
-  constructor(botToken, openai) {
+  constructor(botToken, openai, messageHandler, guildService) {
     super();
     this.botToken = botToken;
     this.openai = openai;
-    this.guildService = new GuildService(this.client);
+    this.guildService = guildService;
+    this.messageHandler = messageHandler;
   }
 
   async init() {
-    this.client = new Client({
+    this.client = this.createClient();
+
+    this.client.on('ready', async () => {
+      console.log(`Logged in as ${this.client.user.tag}!`);
+
+      await this.guildService.registerCommands();
+
+      this.guildService.registerListeners();
+    });
+
+    this.client.on('messageCreate', async (message) => {
+      if (message.author.bot) return;
+
+      if (message.mentions.has(this.client.user)) {
+        await this.messageHandler.handleMentionedMessage(message);
+      }
+    });
+
+    await this.login();
+  }
+
+  createClient() {
+    return new Client({
       intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -25,25 +45,6 @@ class ClientService extends BaseService {
         GatewayIntentBits.GuildPresences,
       ],
     });
-
-    this.client.on('ready', async () => {
-      console.log(`Logged in as ${this.client.user.tag}!`);
-
-      await this.guildService.registerCommands();
-
-      this.guildService.registerListeners();
-
-    });
-
-    this.client.on('messageCreate', async (message) => {
-      if (message.author.bot) return;
-
-      if (message.mentions.has(this.client.user)) {
-        await messageHandler.handleMentionedMessage(message);
-      }
-    });
-
-    await this.login();
   }
 
   async login() {
